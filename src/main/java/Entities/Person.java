3db40ca9d821e116569d42753394dc5f4ef711f1
Zyrotrox <philipp.isola@gmail.com>
2018-11-01 18:14:06 +0100

    added maven, infrastructure

---
+++ src/main/java/Entities/Person.java
@@ -0,0 +1,6 @@
+package Entities;
+
+public class Person {
+    private String handle;
+
+}
---
+++ src/main/java/IO/Discord/Listener.java
@@ -0,0 +1,9 @@
+package IO.Discord;
+
+import IO.IListener;
+
+import java.util.EventListener;
+
+public class Listener implements EventListener, IListener {
+
+}
