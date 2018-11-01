9c40fa7e13b70a50d7308b07a674776143704fa9
Zyrotrox <philipp.isola@gmail.com>
2018-11-01 18:56:04 +0100

    basic discord bot functionality

--- src/main/java/IO/Discord/Listener.java
+++ src/main/java/IO/Discord/Listener.java
@@ -1,12 +1,22 @@
 package IO.Discord;
 
 import IO.IListener;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 
-import java.util.EventListener;
 
-public class Listener implements EventListener, IListener {
+public class Listener implements IListener {
+    private DiscordApi api;
+    private static String token = "MjY1NTUyMDY4NzM4MDIzNDI1.DrzIaQ.OAiIW1GMoFgWEga__NVXHiVNwxY";
 
     public Listener(){
+        this.api = new DiscordApiBuilder().setToken(this.token).login().join();
+
+        this.api.addMessageCreateListener(event -> onMessageReceived(event.getMessageContent()));
+    }
+
+    @Override
+    public void onMessageReceived(String msg) {
 
     }
 }
